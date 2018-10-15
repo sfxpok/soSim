85f3ebbf96cebb64fc1fb9b339ce66e892c7d868
sfxpok <ricardo@airmail.cc>
2018-10-15 19:27:09 +0100

    tentar ler textfiles

--- simulador.c
+++ simulador.c
@@ -1,7 +1,9 @@
 #include "libs.h"
 
-void main () {
+#define MAXFILE 128
 
+void main () {
+    readConfig();
 }
 
 void readConfig() {
@@ -9,16 +11,28 @@ void readConfig() {
     FILE * fileConfig = fopen("config.conf", "r");
     fileConfig = fopen("config.conf", "r");
 
+    char line[256];
+    char param[MAXFILE];
+    int value;
+
     if (fileConfig == NULL) {
         printf("Erro ao abrir o ficheiro. O programa irá usar valores pré-definidos.");
     }
 
     else {
-        printf("A inicializar a simulação...");
-        /* ler o ficheiro de configuração aqui */
+        printf("A ler o ficheiro de configuração...");
+
+        while(fscanf(fileConfig, "%s: %d\n", param, &value) != EOF) {
+            /* ler linha a linha aqui */
+        }
+
+
+        while (fgets(line, sizeof(line), fileConfig)) {
+            printf("%s", line); 
+        }
 
         fclose(fileConfig);
-        printf("Inicialização feita.");
+        printf("\nInicialização feita.");
         
     }
 
