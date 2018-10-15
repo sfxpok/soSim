44d28ee12a3612a175900b36fcf2df1bb1bcfbe4
sfxpok <ricardo@airmail.cc>
2018-10-15 16:52:32 +0100

    texto do menu adicionado

---
+++ monitor.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main() {
+    printMenu();
+}
+
+void printMenu() {
+    printf("┌─────────────────────────────────────────────────────────────┐\n");
+    printf("│                            Loja                             │\n");
+    printf("│                    Sistemas Operativos 18/19                │\n");
+    printf("├─────────────────────────────────────────────────────────────┤\n");
+}
