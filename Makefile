8b026cf34cfe3c39de64a83db0a06c86103e39a7
sfxpok <ricardo@airmail.cc>
2018-11-05 23:06:32 +0000

    makefile ajeitado para monitor e adicionei contributo para a interface

--- Makefile
+++ Makefile
@@ -1,7 +1,10 @@
-all: simulador
+all: simulador monitor
+
+monitor: monitor.o
+	gcc -o monitor monitor.o -lpthread
 
 simulador: simulador.o
 	gcc -o simulador simulador.o -lpthread
 
 clean:
-	rm -f *.o *~ simulador
+	rm -f *.o *~ simulador monitor
--- monitor.c
+++ monitor.c
@@ -3,15 +3,15 @@
 void displayHeader() {
     printf("┌─────────────────────────────────────────────────────────────┐\n");
     printf("│                            Loja                             │\n");
-    printf("│                    Sistemas Operativos 18/19                │\n");
+    printf("│                Sistemas Operativos 2018/2019                │\n");
     printf("├─────────────────────────────────────────────────────────────┤\n");
 }
 
 void displayMenu() {
     printf("┌─────────────────────────────────────────────────────────────┐\n");
-    printf("│                            Loja                             │\n");
+    printf("│                      Lista de operacoes                     │\n");
     printf("├─────────────────────────────────────────────────────────────┤\n");
-    printf("│                    Sistemas Operativos 18/19                │\n");
+    printf("│        O comando é a primeira letra de cada operacao        │\n");
     printf("├─────────────────────────────────────────────────────────────┤\n");
     printf("│(i)nit - inicializa a simulacao                              │\n");
     printf("│(h)alt - pausa a simulacao                                   │\n");
@@ -32,7 +32,7 @@ void pauseShop() {
 }
 
 void closeShop() {
-    //
+    printf("A loja foi fechada.\n");
 }
 
 void initThreads() {
@@ -40,11 +40,14 @@ void initThreads() {
     pthread_t tMonitor;
     // pthread_create(tMonitor, NULL, NULL, NULL);
 
+    printf("PID do monitor: %d\n", getpid());
+
 }
 
 void askForInput() {
 
     char op;
+    int halt = 0;
 
     do {
 
@@ -59,16 +62,25 @@ void askForInput() {
             
             case 'h':
                 break;
+
+            case 'q': // to be fixed
+                halt = 1;
+                break;
             
         }
 
-    } while((op = getchar()) != 'q');
+        // op = getchar();
+
+    } while(halt != 1);
+
+    closeShop();
 
 }
 
 void main() {
     displayHeader();
     displayMenu();
+    askForInput();
 
     // initThreads();
 }
